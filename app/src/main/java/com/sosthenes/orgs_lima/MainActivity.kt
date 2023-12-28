b52a88b198683c9ecf79c8bd38159e46b8362df4
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2023-12-27 23:17:03 -0300

    Commit Views do layout
    Buscando as views do layout na Activy

--- app/src/main/java/com/sosthenes/orgs_lima/MainActivity.kt
+++ app/src/main/java/com/sosthenes/orgs_lima/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import com.sosthenes.orgs_lima.R.id.nome
 
 
 class MainActivity: Activity() {
@@ -11,6 +12,12 @@ class MainActivity: Activity() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
+            val nome = findViewById<TextView>(R.id.nome)
+            nome.setText("cesta de frutas")
+            val descricao = findViewById<TextView>(R.id.descricao)
+            descricao.setText("Laranja, manga, uva")
+            val valor = findViewById<TextView>(R.id.valor)
+            valor.setText("19.99")
     }
 
 }
